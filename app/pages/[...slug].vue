0b7f8d50f2664011a60a819e9d4f03eb3ca1eb05
menra11 <3154975390@qq.com>
2025-12-24 17:57:22 +0800

    更新ProseH1组件，移除不必要的链接生成逻辑；在页面模板中添加文章标题、日期和描述的显示

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -15,7 +15,24 @@ if (!page.value) {
 </script>
 
 <template>
+  <header
+    class="mt-10 px-8 max-w-4xl mx-auto flex flex-col justify-start items-start"
+  >
+    <h1 class="text-xl font-bold">
+      {{ page!.title }}
+    </h1>
+    <div>
+      <span class="text-xs pr-5">{{ page!.date }}</span>
+      <span class="text-xs">{{ page!.description }}</span>
+    </div>
+    <div class="h-1 w-full bg-slate-700 my-1"></div>
+    <div class="h-0.5 w-full bg-slate-700"></div>
+  </header>
   <Contaner>
-      <ContentRenderer v-if="page" :value="page" class="flex flex-col gap-4 max-w-3xl mx-auto"/>
+    <ContentRenderer
+      v-if="page"
+      :value="page"
+      class="flex flex-col gap-4 max-w-3xl mx-auto"
+    />
   </Contaner>
 </template>
