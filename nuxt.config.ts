748bc0a2cb1f9d93ad3ba1aab5d70699c39d1afb
menra11 <3154975390@qq.com>
2025-12-24 23:20:15 +0800

    更新.gitignore，添加.vercel和.env*.local；更新nuxt.config.ts，确保SSR设置为true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/eslint'],
+  ssr: true,
   css: ['~/assets/css/main.css'],
   vite:{
     plugins: [tailwindcss()],
