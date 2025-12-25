bdd6d056759f12a31c179a831cda61393dbd584b
menra11 <3154975390@qq.com>
2025-12-25 14:20:02 +0800

    feat(api): add endpoint to retrieve database version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,25 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxt/eslint'],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxthub/core'],
   devtools: { enabled: true },
   // ssr: true,
   css: ['~/assets/css/main.css'],
+  content: {
+    database: {
+      type: 'postgresql',
+      url: process.env.DATABASE_URL!,
+    },
+  },
   compatibilityDate: '2024-04-03',
+  hub: {
+    db: {
+      dialect: 'postgresql',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+      },
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
